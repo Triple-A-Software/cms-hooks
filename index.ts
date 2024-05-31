f5a5be8065254cafe45257957b2f9cf1ea013760
Blechlawine <marczinser@gmx.de>
2024-05-31 15:10:10 +0200

    feat: initial commit with a few hook names

---
+++ index.ts
@@ -0,0 +1,10 @@
+export enum HookName {
+	PAGE_CACHE_UPDATE = "pageCache.update",
+	PAGE_CACHE_UPDATE_ALL = "pageCache.updateAll",
+	RENDERER_BEFORE_PAGE_COMPILE = "renderer.page.compile.before",
+	RENDERER_RENDER_PAGE = "renderer.page.render",
+	RENDERER_BEFORE_ELEMENT_COMPILE = "renderer.element.compile.before",
+	RENDERER_RENDER_ELEMENT = "renderer.element.render",
+	RENDERER_BEFORE_GENERATE_UNOCSS = "renderer.unocss.generate.before",
+}
+
